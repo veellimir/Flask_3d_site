6190b143e82f0d313272f6ea0f0e8b0dee9f7dd7
vellimir <kkazancov@gmail.com>
2024-01-02 23:40:26 +0300

    Add slider

---
+++ static/js/slider-index.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 10,
+      stretch: 0,
+      depth: 230,
+      modifier: 5,
+      slideShadows: true
+    },
+    keyboard: {
+      enabled: true
+    },
+    mousewheel: {
+      thresholdDelta: 70
+    },
+    spaceBetween: 30,
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
+  
